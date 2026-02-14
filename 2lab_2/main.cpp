49ba2c0c094b268742f69bc6a7153820d49c4f6e
Shmapng <ewgolushko@gmail.com>
2026-02-14 14:25:16 +0200

    Create main.cpp

---
+++ 2lab_2/main.cpp
@@ -0,0 +1,47 @@
+#include "Car.h"
+
+int main() {
+    setlocale(LC_ALL, "");
+
+
+    srand(time(nullptr));
+
+
+    vector<string> veshchi1 = {"a", "b", "v"};
+    vector<string> veshchi2 = {"v", "t", "k"};
+
+    Avtomobil mashina1("A", "ModelA", "KuzovA", "a777aa", 100, veshchi1);
+    Avtomobil mashina2("B", "ModelB", "KuzovB", "b777bb", 200, veshchi2);
+
+    cout << "=== Mashina 1 ===";
+    mashina1.pokazatInfo();
+    cout << "\n=== Mashina 2 ===";
+    mashina2.pokazatInfo();
+
+    // Оп +
+    Avtomobil rezultatPlus = mashina1 + mashina2;
+    cout << "\n=== Rezultat '+' ===";
+    rezultatPlus.pokazatInfo();
+
+    // Оп -
+    Avtomobil rezultatMinus = mashina1 - mashina2;
+    cout << "\n=== Rezultat '-' ===";
+    rezultatMinus.pokazatInfo();
+
+    //  Оп /
+    Avtomobil rezultatDelenie = mashina1 / mashina2;
+    cout << "\n=== Rezultat '/' ===";
+    rezultatDelenie.pokazatInfo();
+
+    
+    Avtomobil mashina3 = mashina1; 
+    cout << "\n=== Kopiya mashina1 (mashina3) ===";
+    mashina3.pokazatInfo();
+
+    Avtomobil mashina4;
+    mashina4 = mashina2; 
+    cout << "\n=== Prisvoenie mashina2 (mashina4) ===";
+    mashina4.pokazatInfo();
+
+    return 0;
+}
