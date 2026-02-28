0abe3e35f9e88297b78cade28aaefa65c5120903
Shmapng <ewgolushko@gmail.com>
2026-02-28 12:32:35 +0200

    Update main.cpp

--- 2lab_2/main.cpp
+++ 2lab_2/main.cpp
@@ -1,47 +1,80 @@
-#include "Car.h"
+#include "Car.hpp"
+#include "AutoService.hpp"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void otsenka() {
+    cout << "\n--- ocenka raboty ---" << endl;
+    cout << "interes: ∞/10" << endl;
+    cout << "napolnennost: 1000/10" << endl;
+    cout << "slozhnost: 777/10" << endl;
+}
 
 int main() {
     setlocale(LC_ALL, "");
-
-
     srand(time(nullptr));
-
-
-    vector<string> veshchi1 = {"a", "b", "v"};
-    vector<string> veshchi2 = {"v", "t", "k"};
-
-    Avtomobil mashina1("A", "ModelA", "KuzovA", "a777aa", 100, veshchi1);
-    Avtomobil mashina2("B", "ModelB", "KuzovB", "b777bb", 200, veshchi2);
-
-    cout << "=== Mashina 1 ===";
-    mashina1.pokazatInfo();
-    cout << "\n=== Mashina 2 ===";
-    mashina2.pokazatInfo();
-
-
-    Avtomobil rezultatPlus = mashina1 + mashina2;
-    cout << "\n=== Rezultat '+' ===";
-    rezultatPlus.pokazatInfo();
-
-
-    Avtomobil rezultatMinus = mashina1 - mashina2;
-    cout << "\n=== Rezultat '-' ===";
-    rezultatMinus.pokazatInfo();
-
-
-    Avtomobil rezultatDelenie = mashina1 / mashina2;
-    cout << "\n=== Rezultat '/' ===";
-    rezultatDelenie.pokazatInfo();
-
     
-    Avtomobil mashina3 = mashina1; 
-    cout << "\n=== Kopiya mashina1 (mashina3) ===";
-    mashina3.pokazatInfo();
-
-    Avtomobil mashina4;
-    mashina4 = mashina2; 
-    cout << "\n=== Prisvoenie mashina2 (mashina4) ===";
-    mashina4.pokazatInfo();
-
+    cout << "laboratornaya 3" << endl;
+    
+    cout << "\n--- mashiny ---" << endl;
+    vector<string> vesh1 = {"aptechka", "ognetushitel", "zapaska"};
+    vector<string> vesh2 = {"zaryadka", "magnitola", "buket"};
+    vector<string> vesh3 = {"zont", "ochki", "karta"};
+    
+    Avtomobil m1("Toyota", "Camry", "JTDBE32K123456789", "A123BC", 45000, vesh1);
+    Avtomobil m2("BMW", "X5", "WBA1234567890ABC", "B456CD", 32000, vesh2);
+    Avtomobil m3("Lada", "Vesta", "XTA1234567890XYZ", "C789EF", 15000, vesh3);
+    
+    cout << "\n--- masterskaya ---" << endl;
+    Avtomasterskaya ms("ul. lenina, 15, moskva");
+    
+    cout << "\n--- magnitoly ---" << endl;
+    Magnitola mg1("Pioneer DEH-80PRS", "CD/MP3 receiver", 25990.0);
+    Magnitola mg2("Sony MEX-XB120B", "digital receiver", 18490.0);
+    Magnitola mg3("JVC KD-X372BT", "CD receiver", 9990.0);
+    
+    ms.dobavitMagnitolu(mg1);
+    ms.dobavitMagnitolu(mg2);
+    ms.dobavitMagnitolu(mg3);
+    
+    cout << "\n--- prikreplyaem mashiny ---" << endl;
+    ms.prikrepitMashinu(&m1);
+    ms.prikrepitMashinu(&m2);
+    ms.prikrepitMashinu(&m3);
+    
+    ms.pokazatInfo();
+    
+    cout << "\n--- ustanavlivaem magnitoly ---" << endl;
+    ms.prikruitMagnitoluKMashine("A123BC", "Pioneer DEH-80PRS");
+    ms.prikruitMagnitoluKMashine("B456CD", "Sony MEX-XB120B");
+    
+    cout << "\n--- informaciya o mashinah ---" << endl;
+    m1.pokazatInfo();
+    m2.pokazatInfo();
+    m3.pokazatInfo();
+    
+    cout << "\n--- oshibka ustanovki ---" << endl;
+    ms.prikruitMagnitoluKMashine("C789EF", "Blaupunkt");
+    
+    ms.pokazatVseMagnitoly();
+    
+    cout << "\n--- udalyaem magnitolu ---" << endl;
+    ms.udalitMagnitolu("JVC KD-X372BT");
+    
+    cout << "\n--- otkreplyaem mashinu ---" << endl;
+    ms.otkrepitMashinu("B456CD");
+    
+    ms.pokazatInfo();
+    
+    cout << "\n--- kopirovanie ---" << endl;
+    Avtomasterskaya ms2 = ms;
+    ms2.ustanovitAdres("pr. mira, 30, spb");
+    ms2.pokazatInfo();
+    
+    otsenka();
+    cout << "\n=== konec ===" << endl;
+    
     return 0;
 }
